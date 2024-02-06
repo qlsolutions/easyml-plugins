6549ec014a1409ababc46bc9f2466cc3cce7ee0e
Denis Mehilli <denis.mehilli@qlsol.com>
2024-02-06 17:22:16 +0100

    redefined HookContext

--- pluginservice/src/main/java/com/quicklink/pluginservice/hooks/HookContext.java
+++ pluginservice/src/main/java/com/quicklink/pluginservice/hooks/HookContext.java
@@ -8,23 +8,56 @@ import org.jetbrains.annotations.NotNull;
 
 public class HookContext {
 
-  private final int idApp;
-  private final String nameApp;
+  private final Integer modelId;
+  private final Long timestamp;
+  private final String status;
+  private final Double minPredicted;
+  private final Double maxPredicted;
+  private final Double observer;
+
   private final Map<String, ?> parameters;
 
 
-  public HookContext(int idApp, String nameApp, Map<String, ?> parameters) {
-    this.idApp = idApp;
-    this.nameApp = nameApp;
+  public HookContext(
+      Integer modelId,
+      Long timestamp,
+      String status,
+      Double minPredicted,
+      Double maxPredicted,
+      Double observer,
+      Map<String, ?> parameters
+  ) {
     this.parameters = parameters;
+    this.modelId = modelId;
+    this.timestamp = timestamp;
+    this.status = status;
+    this.minPredicted = minPredicted;
+    this.maxPredicted = maxPredicted;
+    this.observer = observer;
   }
 
-  public int idApp() {
-    return idApp;
+  public Integer getModelId() {
+    return modelId;
   }
 
-  public String nameApp() {
-    return nameApp;
+  public Long getTimestamp() {
+    return timestamp;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public Double getMinPredicted() {
+    return minPredicted;
+  }
+
+  public Double getMaxPredicted() {
+    return maxPredicted;
+  }
+
+  public Double getObserver() {
+    return observer;
   }
 
   public <T> T param(@NotNull KeyParam<T> key) {
