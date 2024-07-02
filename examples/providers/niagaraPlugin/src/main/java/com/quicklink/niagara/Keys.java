1f1dd057c39180b11e593eb3fcdc9288afde7a55
Denis Mehilli <denis.mehilli@qlsol.com>
2024-07-02 11:22:58 +0200

    updated NiagaraPlugin keys default value

--- examples/providers/niagaraPlugin/src/main/java/com/quicklink/niagara/Keys.java
+++ examples/providers/niagaraPlugin/src/main/java/com/quicklink/niagara/Keys.java
@@ -4,9 +4,9 @@ package com.quicklink.niagara;
 import com.quicklink.easyml.plugins.api.KeyParam;
 
 public final class Keys {
-  static KeyParam<String> PROTOCOL = KeyParam.of("protocol", "http");
+  static KeyParam<String> PROTOCOL = KeyParam.of("protocol", "https");
   static KeyParam<String> HOST = KeyParam.of("host", "192.168.1.1");
-  static KeyParam<Integer> PORT = KeyParam.of("port", 8080);
-  static KeyParam<String> USERNAME = KeyParam.of("username", "energylink_debug");
+  static KeyParam<Integer> PORT = KeyParam.of("port", 443);
+  static KeyParam<String> USERNAME = KeyParam.of("username", "admin");
   static KeyParam<String> PASSWORD = KeyParam.ofSecret("password", "");
 }
