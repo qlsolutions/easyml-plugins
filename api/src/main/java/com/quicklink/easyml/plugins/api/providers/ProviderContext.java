5a5c2fc4b15d27adb37964b2c6121c8e884f0c9d
Denis Mehilli <denis.mehilli@qlsol.com>
2024-10-31 14:34:40 +0100

    0.8.0 - date iterator is done on the server api

--- api/src/main/java/com/quicklink/easyml/plugins/api/providers/ProviderContext.java
+++ api/src/main/java/com/quicklink/easyml/plugins/api/providers/ProviderContext.java
@@ -27,23 +27,11 @@ public class ProviderContext {
   private final String providerName;
   private final Map<String, ?> parameters;
 
-  @Nullable
-  private final Instant start;
-  @Nullable
-  private final Instant end;
-
-  public ProviderContext(ProviderPlugin plugin, @NotNull UUID providerId, @NotNull String providerName, @NotNull Map<String, ?> parameters,
-      @Nullable Instant start, @Nullable Instant end) {
+  public ProviderContext(ProviderPlugin plugin, @NotNull UUID providerId, @NotNull String providerName, @NotNull Map<String, ?> parameters) {
     this.plugin = plugin;
     this.providerId = providerId;
     this.providerName = providerName;
     this.parameters = parameters;
-    this.start = start;
-    this.end = end;
-  }
-
-  public ProviderContext( ProviderPlugin plugin, UUID providerId, @NotNull String nameApp, @NotNull Map<String, ?> parameters) {
-    this(plugin, providerId, nameApp, parameters, null, null);
   }
 
   public UUID providerId() {
@@ -56,25 +44,6 @@ public class ProviderContext {
 
   public @Nullable <T> T param(@NotNull Parameter<T> key) {
     return (T) parameters.getOrDefault(key.key(), null);
-  }
-
-  public int limit() {
-    if(!plugin.isLimitParameterSupported()) {
-      throw new UnsupportedOperationException("This plugin does not support the limit parameter");
-    }
-    return (int) parameters.get("limit");
-  }
-
-  public Stream<DateRange> dateRangeStream(int field) {
-    if (start == null || end == null) {
-      throw new UnsupportedOperationException("Start ts or end ts not set");
-    }
-    Calendar calendar = Calendar.getInstance();
-    calendar.setTimeInMillis(start.toEpochMilli());
-
-
-    var iterator = new DateRangeIterator(calendar, new Date(end.toEpochMilli()), limit(), field);
-    return StreamUtils.asStream(iterator);
   }
 
 }
