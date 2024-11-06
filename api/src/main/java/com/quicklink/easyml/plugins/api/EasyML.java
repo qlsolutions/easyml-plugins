dde79dd35b710d2ca2f69a918a5b549ff89aee4a
Denis Mehilli <denis.mehilli@qlsol.com>
2024-11-06 15:06:24 +0100

    0.8.3 - added default ok http client

--- api/src/main/java/com/quicklink/easyml/plugins/api/EasyML.java
+++ api/src/main/java/com/quicklink/easyml/plugins/api/EasyML.java
@@ -5,8 +5,8 @@
 package com.quicklink.easyml.plugins.api;
 
 import com.quicklink.easyml.plugins.api.json.JsonMapper;
+import okhttp3.OkHttpClient;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
 
 /**
  * EasyMLPlugins - Insert description here.
@@ -15,24 +15,39 @@ import org.slf4j.Logger;
  * @creation 17/10/2024
  */
 public final class EasyML {
+
   private EasyML() {
 
   }
 
   private static Server server;
   private static JsonMapper jsonMapper;
+  private static OkHttpClient httpClient;
+
+  private static void init(@NotNull Server server, @NotNull JsonMapper jsonMapper, @NotNull OkHttpClient httpClient) {
+    EasyML.server = server;
+    EasyML.jsonMapper = jsonMapper;
+    EasyML.httpClient = httpClient;
+  }
 
   public static @NotNull Server getServer() {
-    if(server == null) {
+    if (server == null) {
       throw new IllegalStateException("Error retrieving server from plugin");
     }
     return server;
   }
 
   public static @NotNull JsonMapper getJsonMapper() {
-    if(jsonMapper == null) {
+    if (jsonMapper == null) {
       throw new IllegalStateException("Error retrieving jsonMapper from plugin");
     }
     return jsonMapper;
+  }
+
+  public static @NotNull OkHttpClient getHttpClient() {
+    if(httpClient == null) {
+      throw new IllegalStateException("Error retrieving httpClient from plugin");
+    }
+    return httpClient;
   }
 }
